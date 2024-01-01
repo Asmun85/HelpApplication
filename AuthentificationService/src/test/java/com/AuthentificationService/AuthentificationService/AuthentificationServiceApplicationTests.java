cfc2caf1e1d2923117a78a67d154dd16dffc4e32
AsmunK <asmunkanane1@gmail.com>
2024-01-01 21:45:36 +0100

    initial project

---
+++ AuthentificationService/src/test/java/com/AuthentificationService/AuthentificationService/AuthentificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AuthentificationService.AuthentificationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthentificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ RequestManagementService/src/main/java/com/RequestManagementService/RequestManagementService/RequestManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.RequestManagementService.RequestManagementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestManagementServiceApplication.class, args);
+	}
+
+}
